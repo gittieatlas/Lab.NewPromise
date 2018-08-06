66c711a397ec555128d24a7903ffb964310512f5
Gittie Atlas <gittieatlas@gmail.com>
2018-08-06 11:56:54 -0400

    Appends fulfillment handler to the sleep promise

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 function sleep(waitTime) {
-  return new Promise((resolve, reject) => {
+  const sleepPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('Sucess');
     }, waitTime);
   });
+
+  sleepPromise.then(successMessage => {
+    // successMessage is whatever we passed in the resolve(...) function above.
+    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+    console.log('Yay! ' + successMessage);
+  });
+
+  return sleepPromise;
 }
 
 async function init() {
